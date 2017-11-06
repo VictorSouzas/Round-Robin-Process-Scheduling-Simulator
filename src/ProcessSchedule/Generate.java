b82b818d7c26a46b70edd6e279ac188233c57d76
Victor Souza <victor_souzas@yahoo.com.br>
2017-11-06 04:02:28 -0200

    All tho be static i think that what they do is so little and yet so important. generate a process queue using randomization of the task and also deciding when it has a io operation and insert at least one value in that I/O operations.

--- src/ProcessSchedule/Generate.java
+++ src/ProcessSchedule/Generate.java
@@ -1,11 +1,29 @@
 package ProcessSchedule;
 
 import DataStructure.Queue;
+import Process.Process;
 
 public class Generate {
 
-    public static void process(int max){
+    public static Queue process(int max){
         Queue queue = new Queue();
+        for (int i = 0; i < max; i++){
+            int duration = (int) (Math.random() * 100);
+            if(Math.random() < 0.5){
+                queue.add(new Process(i,duration));
+                continue;
+            }
+            queue.add(new Process(i,duration, Generate.generateIoQueue(duration)));
+        }
+        return queue;
+    }
+
+    private static Queue generateIoQueue(int duration){
+        Queue queue = new Queue(Math.random() * duration);
+        for (int i = 0; i < duration; i++)
+            if(Math.random() < 0.5)
+                queue.add(Math.random() * duration);
+        return queue;
     }
 
 }
