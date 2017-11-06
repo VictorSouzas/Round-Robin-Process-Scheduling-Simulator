5cfc5d2b3dda6f2410523462c6891a305b8e313a
Victor Souza <victor_souzas@yahoo.com.br>
2017-11-06 02:00:51 -0200

    The night is dark and full of statics and this was only the first one thinking in making all events statics arrive created #gambi

---
+++ src/ProcessSchedule/Event.java
@@ -0,0 +1,19 @@
+package ProcessSchedule;
+
+import DataStructure.Queue;
+import Process.Process;
+public class Event {
+    public static Queue arrive(Queue processQueue, int time){
+        int size = processQueue.getSize();
+        Queue newQueue = new Queue();
+        for (int i = 0; i <= size; i++){
+            Process process = (Process) processQueue.remove();
+            if((int) process.getInput().getFirst() == time){
+                newQueue.add(process);
+                continue;
+            }
+            processQueue.add(process);
+        }
+        return newQueue;
+    }
+}
