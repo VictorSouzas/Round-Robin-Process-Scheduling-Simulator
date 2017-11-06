3e75faefc0e7af0b8dc606c50d3cb1edcbf2f7a6
Victor Souza <victor_souzas@yahoo.com.br>
2017-11-06 01:43:26 -0200

    inserted attribute arrival in class

--- src/Process/Process.java
+++ src/Process/Process.java
@@ -1,4 +1,49 @@
 package Process;
 
+import DataStructure.Queue;
+
 public class Process {
+    private int pid;
+    private int duration;
+    private int arrival;
+    private Queue input;
+    private Queue output;
+    private Queue IO;
+
+    public Process(int pid, int duration, int arrival) {
+        this.pid = pid;
+        this.duration = duration;
+        this.arrival = arrival;
+        this.input = new Queue();
+        this.output = new Queue();
+    }
+
+    public Process(int pid, int duration, int arrival, Queue IO){
+        this(pid, duration, arrival);
+        this.IO = IO;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getArrival() {
+        return arrival;
+    }
+
+    public Queue getIO() {
+        return IO;
+    }
+
+    public Queue getInput() {
+        return input;
+    }
+
+    public Queue getOutput() {
+        return output;
+    }
 }
