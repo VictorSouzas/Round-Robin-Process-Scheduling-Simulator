6e2e7c7d604690d9f28ba8949900e3f451ed90bc
victor <victor_souzas@yahoo.com.br>
2018-02-08 21:31:26 -0200

    reviewing the entire project

--- src/ProcessSchedule/Main.java
+++ src/ProcessSchedule/Main.java
@@ -1,11 +1,8 @@
 package ProcessSchedule;
 
-import DataStructure.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue waitingQueue = Generate.process(Integer.parseInt(args[0]));
-        RobinRound robinRound = new RobinRound(waitingQueue);
-        robinRound.getProcessedQueue();
+
     }
 }
