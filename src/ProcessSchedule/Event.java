b0cec0b68160c1b80c7d6b5de49fb45224663cd1
victor <victor_souzas@yahoo.com.br>
2018-02-09 18:57:05 -0200

    arrive wrong implemented

--- src/ProcessSchedule/Event.java
+++ src/ProcessSchedule/Event.java
@@ -6,14 +6,14 @@ public class Event {
     public static Queue arrive(Queue processQueue, int time){
         int size = processQueue.getSize();
         Queue newQueue = new Queue();
-        for (int i = 0; i < size; i++){
+        /*for (int i = 0; i <= size; i++){
             Process process = (Process) processQueue.remove();
             if((int) process.getInput().getFirst().getValue() == time){
                 newQueue.add(process);
                 continue;
             }
             processQueue.add(process);
-        }
+        }*/
         return newQueue;
     }
 
